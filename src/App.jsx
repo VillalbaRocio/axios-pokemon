5c213102f2d375efda3fe465c302029c1725408c
Villalba Rocio <rocio.meneses99@gmail.com>
2024-07-03 22:30:53 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [listaPokemon, setlistaPokemon] = useState([]);
+
+      const cargarPokemon = async () => {
+        const URL = "https://pokeapi.co/api/v2/pokemon/?limit=807";
+  
+        try{
+          const respuesta = await axios.get(URL);
+          setlistaPokemon(respuesta.data.results);
+        }
+        catch(error){
+          console.log("Ocurrió un error", error);
+        }
+      }
+  
+
+
+  return (
+    <div className="App">
+      <div className="contenedor-pokemon">
+        <button className="btn" onClick={cargarPokemon}>Fetch Pokemon</button>
+        <ul>
+          {listaPokemon.map((pokemon, index) => (
+            <li key={index}>{pokemon.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
